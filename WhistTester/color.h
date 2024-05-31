ccf892bb198861cce2de2eb980b14940d9be59ab
lbadea1000 <126151689+lbadea1000@users.noreply.github.com>
2024-05-31 22:54:27 +0300

    Added a little bit of 'spice' :))
    Big card designs with colors

---
+++ WhistTester/color.h
@@ -0,0 +1,9 @@
+#define NO_CARDS 13
+#define CARD_HEIGHT 13
+#define CARD_WIDTH 15
+#define HEARTS 'H'
+#define DIAMONDS 'D'
+#define SPADES 'S'
+#define CLUBS 'C'
+
+void PrintCard(char cardValue, char cardColor);
